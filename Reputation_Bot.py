d5714403a4ff6fa74f258e9c86add3f0c2f3209f
Ryan Baker <mnbakerboy1@gmail.com>
2023-08-25 01:12:13 -0400

    initial commit

---
+++ Reputation_Bot.py
@@ -0,0 +1,63 @@
+﻿import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+CLIENT = os.getenv("CLIENT")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+rep_client = MongoClient(CLIENT)
+db = rep_client.ChroniclesOfArcane
+user_rep = db.UserRep
+
+#Key symbol to identify the start of a command
+bot = commands.Bot(command_prefix='!', intents = intents)
+
+@bot.command(name = "addfeedback")
+async def add_feedback(ctx, user: discord.User, feedback, *notes):
+    """
+    Adds feedback for a user, if the user does not exist in the database, add a new entry for the user.
+    :param user: User being tracked
+    :param feedback: positive/negative
+    :param *notes: Optional notes about transaction
+    """
+
+    if (feedback == "positive" or feedback == "negative"):
+        await ctx.message.add_reaction('✅')
+        combined_notes = ' '.join(notes)
+
+        if (user_rep.find_one({"id": user.id})):
+            user_rep.update({"id": user.id}, {"$inc": {"rep": 1 if feedback == "positive" else -1}})
+            if (combined_notes):
+                user_rep.update({"id": user.id}, {"$push": {"notes": combined_notes}})
+        else:
+            user_rep.insert_one({"id": user.id, "rep": 1 if feedback == "positive" else -1, "notes": combined_notes})
+    else:
+        await ctx.message.add_reaction('❌')
+        await ctx.send("Missing feedback (positive/negative)")
+
+@add_feedback.error
+async def feedback_error(ctx, error):
+    """
+    If the user sending the message doesn't use @ before the reported user's name, it will throw an error
+    :param error: UserNotFound
+    """
+
+    await ctx.message.add_reaction('❌')
+    await ctx.send("User not found")
+
+@bot.command(name = "getfeedback")
+async def get_feedback(ctx, user):
+    """
+    If the user exists in the database, output their name and score
+    :param user: User to retrieve
+    """
+
+    await ctx.send(user)
+
+bot.run(TOKEN)
