a28cbbf058d50fda9e837ccc3724bd1e919f1c9a
Ryan Baker <mnbakerboy1@gmail.com>
2023-08-25 11:39:45 -0400

    Fix the way notes was stored. Added ability to get a user's reputation.

--- Reputation_Bot.py
+++ Reputation_Bot.py
@@ -28,15 +28,16 @@ async def add_feedback(ctx, user: discord.User, feedback, *notes):
     """
 
     if (feedback == "positive" or feedback == "negative"):
-        await ctx.message.add_reaction('✅')
         combined_notes = ' '.join(notes)
 
         if (user_rep.find_one({"id": user.id})):
-            user_rep.update({"id": user.id}, {"$inc": {"rep": 1 if feedback == "positive" else -1}})
+            user_rep.update_one({"id": user.id}, {"$inc": {"rep": 1 if feedback == "positive" else -1}})
             if (combined_notes):
-                user_rep.update({"id": user.id}, {"$push": {"notes": combined_notes}})
+                user_rep.update_one({"id": user.id}, {"$push": {"notes": combined_notes}})
         else:
-            user_rep.insert_one({"id": user.id, "rep": 1 if feedback == "positive" else -1, "notes": combined_notes})
+            user_rep.insert_one({"id": user.id, "rep": 1 if feedback == "positive" else -1, "notes": [combined_notes]})
+
+        await ctx.message.add_reaction('✅')
     else:
         await ctx.message.add_reaction('❌')
         await ctx.send("Missing feedback (positive/negative)")
@@ -52,12 +53,18 @@ async def feedback_error(ctx, error):
     await ctx.send("User not found")
 
 @bot.command(name = "getfeedback")
-async def get_feedback(ctx, user):
+async def get_feedback(ctx, user: discord.User):
     """
     If the user exists in the database, output their name and score
     :param user: User to retrieve
     """
 
-    await ctx.send(user)
+    db_user = user_rep.find_one({"id": user.id})
+    await ctx.send(user.display_name + "'s reputation: " + str(db_user["rep"]))
+
+@get_feedback.error
+async def get_error(ctx, error):
+    await ctx.message.add_reaction('❌')
+    await ctx.send("User not found")
 
 bot.run(TOKEN)
