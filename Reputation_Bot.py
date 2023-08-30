ab4464d441a780c7411888fc17db4a9daa2faa21
Ryan Baker <mnbakerboy1@gmail.com>
2023-08-30 19:22:38 -0400

    Made it discernable between the amount of feedbacks entered and the number of positive and negative ones. Changed display of the reputaion score.

--- Reputation_Bot.py
+++ Reputation_Bot.py
@@ -27,15 +27,15 @@ async def add_feedback(ctx, user: discord.User, feedback, *notes):
     :param *notes: Optional notes about transaction
     """
 
-    if (feedback == "positive" or feedback == "negative"):
+    if (feedback.lower() == "positive" or feedback.lower() == "negative"):
         combined_notes = ' '.join(notes)
 
         if (user_rep.find_one({"id": user.id})):
-            user_rep.update_one({"id": user.id}, {"$inc": {"rep": 1 if feedback == "positive" else -1}})
+            user_rep.update_one({"id": user.id}, {"$inc": {"total": 1, "positive": 1 if  feedback == "positive" else 0}})
             if (combined_notes):
                 user_rep.update_one({"id": user.id}, {"$push": {"notes": combined_notes}})
         else:
-            user_rep.insert_one({"id": user.id, "rep": 1 if feedback == "positive" else -1, "notes": [combined_notes]})
+            user_rep.insert_one({"id": user.id, "total": 1, "positive": 1 if feedback == "positive" else 0})
 
         await ctx.message.add_reaction('✅')
     else:
@@ -50,7 +50,7 @@ async def feedback_error(ctx, error):
     """
 
     await ctx.message.add_reaction('❌')
-    await ctx.send("User not found")
+    await ctx.send("User not found. If user is not in the server, please either invite them and resend your feedback or user their User ID.")
 
 @bot.command(name = "getfeedback")
 async def get_feedback(ctx, user: discord.User):
@@ -60,7 +60,13 @@ async def get_feedback(ctx, user: discord.User):
     """
 
     db_user = user_rep.find_one({"id": user.id})
-    await ctx.send(user.display_name + "'s reputation: " + str(db_user["rep"]))
+    if (db_user):
+        total = db_user["total"]
+        positive = db_user["positive"]
+        negative = total - positive
+        await ctx.send("__" + user.display_name + "__\nTotal: " + str(total) + " Positive: " + str(positive) + " Negative: " + str(negative))
+    else:
+        await ctx.send(user.display_name + " is not in our database.")
 
 @get_feedback.error
 async def get_error(ctx, error):
